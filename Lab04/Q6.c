329ce6fe4e1d654769f978fbd88df55b6d6bb123
iamunknowngamer <144406935+iamunknowngamer@users.noreply.github.com>
2023-09-12 06:47:56 -0700

    Update Q6.c

--- Lab04/Q6.c
+++ Lab04/Q6.c
@@ -1,4 +1,14 @@
-#include<stdio.h>  
+/* Programmer: Syed Saif ur Rehman Shah
+* Date: 9/12/2023
+* Description: This is the code for Question 6, only in this part i use help from the web because it was hard for me as its my 1st time coding in c and i dont know all syntax and commands too.
+* Related files: https://github.com/iamunknowngamer/Pf-Fall23/tree/main/Lab03
+*/
+
+//--Include Files--//
+#include <stdio.h>
+#include <stdlib.h>
+//--Global Variables--//
+
 int main()  
 {  
   int n;  
