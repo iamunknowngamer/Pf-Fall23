da6cb49d33525ee7f8d79729f278e27c7a82f154
iamunknowngamer <144406935+iamunknowngamer@users.noreply.github.com>
2023-09-20 15:33:05 +0500

    Update Task10.c

--- Lab05/Task10.c
+++ Lab05/Task10.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 
-int main () {
-    int b1,b2,b3,b4,b5,b6,b7,b8;
-    printf("Enter 8 bit binary number: \n");                                                  // give the input in bits
-    scanf("%d%d%d%d%d%d%d%d", &b1,&b2,&b3,&b4,&b5,&b6,&b7,&b8);                              // will scan each bit you input
-    printf("%d%d%d%d%d%d%d%d", b5,b6,b7,b8,b1,b2,b3,b4);                                    // will change the higher 4 bits with lower 4 bits
-}//end
+int main() {
+	
+	int N,h,l;
+	
+	printf("Enter a 8 bit binary value: \n");
+	
+	scanf("%8d", &N);
+	
+	if (N==11111111 && N==00000000)
+	{
+		printf("Invalid Input");
+	}
+	
+	h = N % 10000;
+	l = N / 10000;
+	
+	printf("%4d %4d", h, l);
+	
+	return 0;
+}
