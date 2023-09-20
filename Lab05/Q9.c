18830a69cc2d7ff074d1dff4feb6d8bb50d3d384
iamunknowngamer <144406935+iamunknowngamer@users.noreply.github.com>
2023-09-20 14:53:32 +0500

    Create Q9.c

---
+++ Lab05/Q9.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main () {
+    int a,b,c;
+    float D,Root1,Root2;
+    printf("Enter the values of a , b and c of a quadratic equation: \n");
+    scanf("%d%d%d",&a,&b,&c);
+    D = (b*b) - (4*a*c);
+    printf("The discriminant is : %f", D);
+    
+    if (D>=0)
+    {
+        printf("\nThe Roots are Real, Calculating the roots... please wait..");
+        Root1 = (-b - sqrt(D))/ (2*a);
+        Root2 = (-b + sqrt(D))/ (2*a);
+        printf("\nThe Roots are : %f %f", Root1, Root2);
+    }
+    else
+    {
+        printf("\nThe Roots are Complex!!");
+    }
+
+}//end
+
