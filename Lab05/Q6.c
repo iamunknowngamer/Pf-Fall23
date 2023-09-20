15a5ed0d9150f6d12ee129edcba1490d113a3872
iamunknowngamer <144406935+iamunknowngamer@users.noreply.github.com>
2023-09-20 13:43:05 +0500

    Update Q6.c

--- Lab05/Q6.c
+++ Lab05/Q6.c
@@ -5,15 +5,16 @@ int main() {
     int bit1,bit2,bit3,bit4,bit5,bit6,bit7,bit8;
     printf("Enter a 8 bit binary number: \n");                                     // give the input in bits
     scanf("%d%d%d%d%d%d%d%d",&bit1,&bit2,&bit3,&bit4,&bit5,&bit6,&bit7,&bit8);    // will scan each bit you input
-    if (bit4 ==1)
+    if (bit5 ==1)
     {
-        bit4 =0;
+        bit5 =0;
     }
-    if (bit7 ==1)
+    if (bit2 ==1)
     {
-        bit7 =0;
+        bit2 =0;
     }
      printf("%d%d%d%d%d%d%d%d",bit1,bit2,bit3,bit4,bit5,bit6,bit7,bit8);
     
     
 }//end main
+
