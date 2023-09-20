1ef21e61799447acc24155266233f4725ea53381
iamunknowngamer <144406935+iamunknowngamer@users.noreply.github.com>
2023-09-20 15:15:25 +0500

    Delete Lab05/Q10.c

--- Lab05/Q10.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main () {
-    int b1,b2,b3,b4,b5,b6,b7,b8;
-    printf("Enter 8 bit binary number: \n");                                                  // give the input in bits
-    scanf("%d%d%d%d%d%d%d%d", &b1,&b2,&b3,&b4,&b5,&b6,&b7,&b8);                              // will scan each bit you input
-    printf("%d%d%d%d%d%d%d%d", b5,b6,b7,b8,b1,b2,b3,b4);                                    // will change the higher 4 bits with lower 4 bits
-}//end
