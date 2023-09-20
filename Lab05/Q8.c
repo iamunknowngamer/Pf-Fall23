cdeafbf1597ff5224a13db3ba1010ce423146786
iamunknowngamer <144406935+iamunknowngamer@users.noreply.github.com>
2023-09-20 14:07:42 +0500

    Update Q8.c

--- Lab05/Q8.c
+++ Lab05/Q8.c
@@ -1,8 +1,3 @@
-// This question, i didnt get the idea either we have to take brightness value as input and tell to what surrondings is the cell phone being exposed too,
-// or either we need to take the surroundings of the user as input then tell the brightness level.
-
-
-
 #include <stdio.h>
 
 int main() {
@@ -12,15 +7,15 @@ int main() {
 	scanf("%d", &N);
 	if (N > 500)
 	{
-    	printf("The cell phone is exposed under sunlight.");                                   
+    	printf("----High Brightness----");                                   
 	}
 	else if ( N>=0 && N<=100)
 	{
-	    printf("The cell phone is being used at evening.");
+	    printf("----Low Brightness----");
 	}
     else if ( N>=100 && N<=500)
     {
-        printf(" The cell phone is under white light.");
+        printf("----Medium Brightness----");
     }
 
     return 0;
