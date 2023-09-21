485d5aaa334dd583b727ce7800b2f1142548d581
iamunknowngamer <144406935+iamunknowngamer@users.noreply.github.com>
2023-09-21 08:30:38 +0500

    Update Q6.c

--- Lab05/Q6.c
+++ Lab05/Q6.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 int main() {
+   
+    int N;
+    printf("Enter a 8 bit binary number: ");
+    scanf("%08d",&N);
     
-    int bit8,bit7,bit6,bit5,bit4,bit3,bit2,bit1;
-    printf("Enter a 8 bit binary number: \n");                                     // give the input in bits 
-    scanf("%d%d%d%d%d%d%d%d",&bit8,&bit7,&bit6,&bit5,&bit4,&bit3,&bit2,&bit1);    // will scan each bit you input
-    if (bit4 ==1 && bit7 ==1)                                                    // the program will only execute if both bits are open as said in question, if both are on only then it will turn them off.
+    if ((((N/1000)%10) == 1) && (((N/1000000)%10) == 1))
     {
-        bit4 =0;
-        bit7 =0;
+        N = N - 1000 - 1000000;
+        printf("%08d", N);
     }
     
-     printf("%d%d%d%d%d%d%d%d",bit8,bit7,bit6,bit5,bit4,bit3,bit2,bit1);
+    return 0;
     
-    
-}//end main
+}
