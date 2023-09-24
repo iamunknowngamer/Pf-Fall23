afd0c01166fb4eaaeb42246310c53501d1abb751
iamunknowngamer <144406935+iamunknowngamer@users.noreply.github.com>
2023-09-24 00:34:01 -0700

    Update Q6.c

--- Lab04/Q6.c
+++ Lab04/Q6.c
@@ -9,41 +9,52 @@
 #include <stdlib.h>
 //--Global Variables--//
 
-int main()  
-{  
-  int n;  
-  
-  printf("Enter number of rows: ");  
-  scanf("%d",&n);  
-  
-  for(int i=1; i<=n; i++)  
-  {  
-     for(int j=i; j<=n; j++)  
-     {  
-       printf(" ");  
-     }  
-     for(int k=1; k<=2*i-1; k++)  
-     {  
-       if(k==1 || k==(2*i-1)) printf("*");  
-       else printf(" ");  
-     }  
-     printf("\n");  
-  }  
-  
-  for(int i=n-1; i>=1; i--)  
-  {  
-     for(int j=n; j>=i; j--)  
-     {  
-       printf(" ");  
-     }  
-     for(int k=1; k<=2*i-1; k++)  
-     {  
-       if(k==1 || k==2*i-1) printf("*");  
-       else printf(" ");  
-     }  
-     printf("\n");  
-  }  
-  
-  return 0;  
-} //end main 
-
+int main() {
+    
+    int N;
+    printf("Enter any number: ");
+    scanf("%d", &N);
+    
+    for (int i=1; i<=N; i++)
+    {
+        for (int j=i; j<=N; j++)
+        {
+            printf(" ");
+        }
+        for (int k=1; k<=2*i-1; k++)
+        {
+            if (k==1 || k==2*i-1)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+       
+        
+        
+    for (int i=N-1; i>=1; i--)
+    {
+        for (int j=N; j>=i; j--)
+        {
+            printf(" ");
+        }
+        for (int k=1; k<=2*i-1; k++)
+        {
+            if (k==1 || k==2*i-1)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        
+        printf("\n");
+    }    
+}
