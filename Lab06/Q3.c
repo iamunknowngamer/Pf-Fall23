17c8c31c39a97c9dc44e8b9c38b30e34b388edf4
iamunknowngamer <144406935+iamunknowngamer@users.noreply.github.com>
2023-10-06 15:00:03 +0500

    Create Q3.c

---
+++ Lab06/Q3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() 
+{
+
+	int a,b,gcd;
+	printf("Enter two numbers: ");
+	scanf("%d%d",&a,&b);
+	
+	if (a>b) gcd=b;
+	else if (b>a) gcd=a;
+	
+	while(1)
+	{
+	
+		if (a%gcd==0)
+		{
+			if (b%gcd==0)
+			{
+				printf("The GCD is: %d", gcd);	
+				break;
+			}
+				
+		}
+			gcd--;
+	}	
+	
+	int lcm = (a*b)/gcd;
+	printf(" , while the LCM is %d", lcm);
+	
+}//end
