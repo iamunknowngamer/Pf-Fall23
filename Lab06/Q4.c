47f8435a3e68ea1fce4b9825c4f5fc85e21e7f3b
iamunknowngamer <144406935+iamunknowngamer@users.noreply.github.com>
2023-10-06 15:52:54 +0500

    Update Q4.c

--- Lab06/Q4.c
+++ Lab06/Q4.c
@@ -21,24 +21,34 @@ int main()
 			{
 				case 0:
 					printf(" zero");
+					break;
 				case 1:
 					printf(" one");
+					break;
 				case 2:
 					printf(" two");
+					break;
 				case 3:
 					printf(" three");
+					break;
 				case 4:
 					printf(" four");
+					break;
 				case 5:
 					printf(" five");
+					break;
 				case 6:
 					printf(" six");
+					break;
 				case 7:
 					printf(" seven");
+					break;
 				case 8:
 					printf(" eight");
+					break;
 				case 9:
 					printf(" nine");
+					break;
 			}
 			
 		}
@@ -49,6 +59,6 @@ int main()
 		}
 	
 	}
-
-}
+	return 0;
+}//end
 
