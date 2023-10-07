6f80841d5c61bf0f9883e6cc6b12b804b5898e48
iamunknowngamer <144406935+iamunknowngamer@users.noreply.github.com>
2023-10-06 23:41:52 -0700

    Create Q5.c

---
+++ Lab06/Q5.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+int main()
+{
+    int i=0;
+    int count=0;
+    while(1)
+    {
+        printf("           %d",i);
+        count = count + 1;
+        if (count==4) break;
+    }
+    printf("\n            ");
+    i=1;count=0;
+    while(1)
+    {
+        printf("           %d",i);
+        count = count + 1;
+        if (count==2) break;
+    }
+    printf("\n");
+    i=2;count=0;
+    while(1)
+    {
+        printf("           %d",i);
+        count = count + 1;
+        if (count==4) break;
+    }
+    printf("\n            ");
+    i=3;count=0;
+    while(1)
+    {
+        printf("           %d",i);
+        count = count + 1;
+        if (count==2) break;
+    }
+    printf("\n");
+    i=4;count=0;
+    while(1)
+    {
+        printf("           %d",i);
+        count = count + 1;
+        if (count==4) break;
+    }
+    printf("\n            ");
+    i=5;count=0;
+    while(1)
+    {
+        printf("           %d",i);
+        count = count + 1;
+        if (count==2) break;
+    }
+    printf("\n");
+    i=6;count=0;
+    while(1)
+    {
+        printf("           %d",i);
+        count = count + 1;
+        if (count==4) break;
+    }
+}//end
