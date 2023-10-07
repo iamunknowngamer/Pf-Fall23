d2634aed27df892a89b888ffd6a76c6f243a937a
iamunknowngamer <144406935+iamunknowngamer@users.noreply.github.com>
2023-10-06 23:48:46 -0700

    Update Q5.c

--- Lab06/Q5.c
+++ Lab06/Q5.c
@@ -65,3 +65,35 @@ int main()
         if (count==4) break;
     }
 }//end
+
+##Alternate easy method:
+#include <stdio.h>
+
+int main()
+{
+    int i=0;
+    int count=0;
+    for(i=0; i<=6; i++)
+    {
+        if(i%2==0)
+        {   
+            count = 0;
+            while(count<4)
+            {
+                printf("         %d",i);
+                count=count+1;
+            }
+        }
+        else if(i%2!=0)
+        {
+            count=0;
+            printf("          ");
+            while(count<2)
+            {
+                printf("         %d",i);
+                count=count+1;
+            }
+        }
+        printf("\n");
+    }
+}//end
