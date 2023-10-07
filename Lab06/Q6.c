97405f9b03bb5617b45c6ceb1bf9ec18d71459c2
iamunknowngamer <144406935+iamunknowngamer@users.noreply.github.com>
2023-10-07 00:57:03 -0700

    Create Q6.c

---
+++ Lab06/Q6.c
@@ -0,0 +1,270 @@
+#include <stdio.h>
+
+int main()
+{   
+    printf(" 'The Matchstick Game' \n");
+    int m=21;
+    int i=0;
+    while(i<m)
+    {
+        i = i+1;
+        printf(" *");
+    }
+    int N;int count=1;
+    printf("\n\n Please choose a number from 1 to 4: ");
+    scanf("%d",&N);
+    if (N<=4)
+    {
+        switch (N)
+        {
+            case 1:
+                m=21;
+                i=1;
+                while(i<m)
+                {
+                    i = i+1;
+                    printf(" *");
+                }
+                sleep (1);
+                printf("\n The consule is taking his turn wait...");
+                break;
+            case 2:
+                m=21;
+                i=2;
+                while(i<m)
+                {
+                    i = i+1;
+                    printf(" *");
+                }
+                sleep (1);
+                printf("\n The consule is taking his turn wait...");
+                break;
+            case 3:
+                m=21;
+                i=3;
+                while(i<m)
+                {
+                    i = i+1;
+                    printf(" *");
+                }
+                sleep (1);
+                printf("\n The consule is taking his turn wait...");
+                break;
+            case 4:
+                m=21;
+                i=4;
+                while(i<m)
+                {
+                    i = i+1;
+                    printf(" *");
+                }
+                sleep (1);
+                printf("\n The consule is taking his turn wait...");
+                break;
+                
+        }
+        printf("\n");
+        sleep (1);
+        m=21;
+        i=5;
+        while(i<m)
+        {
+            i = i+1;
+            printf(" *");
+        }
+    }
+    else printf("Error! Restart the game");
+    printf("\n\n Ur turn choose from 1 to 4: ");
+    scanf("%d",&N);
+    if (N<=4)
+    {
+        switch (N)
+        {
+            case 1:
+                m=21;
+                i=6;
+                while(i<m)
+                {
+                    i = i+1;
+                    printf(" *");
+                }
+                sleep (1);
+                printf("\n The consule is taking his turn wait...");
+                break;
+            case 2:
+                m=21;
+                i=7;
+                while(i<m)
+                {
+                    i = i+1;
+                    printf(" *");
+                }
+                sleep (1);
+                printf("\n The consule is taking his turn wait...");
+                break;
+            case 3:
+                m=21;
+                i=8;
+                while(i<m)
+                {
+                    i = i+1;
+                    printf(" *");
+                }
+                sleep (1);
+                printf("\n The consule is taking his turn wait...");
+                break;
+            case 4:
+                m=21;
+                i=9;
+                while(i<m)
+                {
+                    i = i+1;
+                    printf(" *");
+                }
+                sleep (1);
+                printf("\n The consule is taking his turn wait...");
+                break;
+                
+        }
+        printf("\n");
+        sleep (1);
+        m=21;
+        i=10;
+        while(i<m)
+        {
+            i = i+1;
+            printf(" *");
+        }
+    }
+    else printf("Error! Restart the game");
+    printf("\n\n Ur turn choose from 1 to 4: ");
+    scanf("%d",&N);
+    if (N<=4)
+    {
+        switch (N)
+        {
+            case 1:
+                m=21;
+                i=11;
+                while(i<m)
+                {
+                    i = i+1;
+                    printf(" *");
+                }
+                sleep (1);
+                printf("\n The consule is taking his turn wait...");
+                break;
+            case 2:
+                m=21;
+                i=12;
+                while(i<m)
+                {
+                    i = i+1;
+                    printf(" *");
+                }
+                sleep (1);
+                printf("\n The consule is taking his turn wait...");
+                break;
+            case 3:
+                m=21;
+                i=13;
+                while(i<m)
+                {
+                    i = i+1;
+                    printf(" *");
+                }
+                sleep (1);
+                printf("\n The consule is taking his turn wait...");
+                break;
+            case 4:
+                m=21;
+                i=14;
+                while(i<m)
+                {
+                    i = i+1;
+                    printf(" *");
+                }
+                sleep (1);
+                printf("\n The consule is taking his turn wait...");
+                break;
+                
+        }
+        printf("\n");
+        sleep (1);
+        m=21;
+        i=15;
+        while(i<m)
+        {
+            i = i+1;
+            printf(" *");
+        }
+    }
+    else printf("Error! Restart the game");
+    printf("\n\n Ur turn choose from 1 to 4: ");
+    scanf("%d",&N);
+    if (N<=4)
+    {
+        switch (N)
+        {
+            case 1:
+                m=21;
+                i=16;
+                while(i<m)
+                {
+                    i = i+1;
+                    printf(" *");
+                }
+                sleep (1);
+                printf("\n The consule is taking his turn wait...");
+                break;
+            case 2:
+                m=21;
+                i=17;
+                while(i<m)
+                {
+                    i = i+1;
+                    printf(" *");
+                }
+                sleep (1);
+                printf("\n The consule is taking his turn wait...");
+                break;
+            case 3:
+                m=21;
+                i=18;
+                while(i<m)
+                {
+                    i = i+1;
+                    printf(" *");
+                }
+                sleep (1);
+                printf("\n The consule is taking his turn wait...");
+                break;
+            case 4:
+                m=21;
+                i=19;
+                while(i<m)
+                {
+                    i = i+1;
+                    printf(" *");
+                }
+                sleep (1);
+                printf("\n The consule is taking his turn wait...");
+                break;
+                
+        }
+        printf("\n");
+        sleep (1);
+        m=21;
+        i=20;
+        while(i<m)
+        {
+            i = i+1;
+            printf(" *");
+        }
+    }
+    else printf("Error! Restart the game");
+
+sleep(1);
+printf("\n You lose :(");
+
+}//end
