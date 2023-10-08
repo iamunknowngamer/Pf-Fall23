434b93975d669d822cf43d89d9d8bd637c0b8374
iamunknowngamer <144406935+iamunknowngamer@users.noreply.github.com>
2023-10-08 00:19:38 -0700

    Create Q7.c

---
+++ Lab06/Q7.c
@@ -0,0 +1,24 @@
+/**
+ * Programmer: Syed Saif ur Rehman Shah.
+ * Description: Practicing Programming.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int N,sum;
+    printf("Enter the number of values you want to sum: ");
+    scanf("%d", &N);
+    int arr[N];
+    
+    for (int i=1; i<=N; i++)
+    {
+        printf("\nNumber %d: ", i);
+        scanf("%d", &arr[i]);
+        sum = sum + arr[i];
+    }
+    
+    printf("\nThe sum is: %d", sum);
+
+}//end
