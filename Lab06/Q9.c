7c27f20223c2c144e8ada3cf1918c014129678a9
iamunknowngamer <144406935+iamunknowngamer@users.noreply.github.com>
2023-10-08 01:59:54 -0700

    Create Q9.c

---
+++ Lab06/Q9.c
@@ -0,0 +1,37 @@
+/**
+ * Programmer: Syed Saif ur Rehman Shah.
+ * Description: Practicing Programming.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int N,max,min;
+    printf("Enter the number of elements: ");
+    scanf("%d", &N);
+    int arr[N];
+    printf("Enter %d numbers separated by space: ", N);
+    
+    for(int i=0; i<N; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    
+    max = min = arr[0];
+    
+    for(int j=N-1; j>=0; j--)
+    {
+        if (min > arr[j])
+        {
+            min = arr[j];
+        }   
+        if (max < arr[j])
+        {
+            max = arr[j];
+        }
+    }
+    
+    printf("Maximun number: %d\nMinimun number: %d", max , min);
+
+}//end
