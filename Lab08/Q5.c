036069fcf1e4d692d8f1a4c2fd03f3e8c4acaa95
iamunknowngamer <144406935+iamunknowngamer@users.noreply.github.com>
2023-10-24 10:52:53 +0500

    Create Q5.c

---
+++ Lab08/Q5.c
@@ -0,0 +1,33 @@
+/* Programmer: Syed Saif ur Rehman Shah
+* Date: 10/10/2023
+* Description: This is the code for Question 1
+* Related files: https://github.com/iamunknowngamer/Pf-Fall23/blob/main/Lab06/Q6.c
+*/
+
+#include <stdio.h>
+
+int carusage(int carnumber, int day)
+{
+    if (carnumber%2==0 && day%2==0 || carnumber%2!=0 && day%2!=0)
+    {
+        return 1;
+    }
+    else return 0;
+}
+int main()
+{
+    int carnumber,day;
+    printf("Enter the numeric value of ur car's number: ");
+    scanf("%d", &carnumber);
+    
+    printf("Enter the day of the week(1 to 7): ");
+    scanf("%d", &day);
+    
+    carusage(carnumber,day);
+    
+    if(carusage(carnumber,day)==1)
+    {
+        printf("You should use your car today.");
+    }
+    else printf("You should not use your car today.");
+}//end
