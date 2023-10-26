6a5dfffdc21d73bfc02517f8bd982559b5007a42
iamunknowngamer <144406935+iamunknowngamer@users.noreply.github.com>
2023-10-26 11:48:45 +0500

    Update Q4.c

--- Lab07/Q4.c
+++ Lab07/Q4.c
@@ -5,62 +5,72 @@
 */
 
 #include <stdio.h>
+int isSymmetric(int m[10][10], int c, int r)
+{
+    int t[10][10];
+    
+    for(int i=0; i<r; i++)
+    {
+        for (int j=0; j<c; j++)
+        {
+            t[j][i] = m[i][j];
+        }
+    }
+    
+    for(int i=0; i<r; i++)
+    {
+        for (int j=0; j<c; j++)
+        {
+            if(t[i][j]!=m[i][j])
+            {
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
 
 int main()
 {
-    int m[3][3], t[3][3];
-    printf("Enter the elements of the matrix (3x3) separated by space: ");
-    for(int i=0; i<3; i++)
+    int m[10][10];
+    int c,r;
+    printf("Enter the number of rows: ");
+    scanf("%d", &r);
+    
+    printf("Enter the number of columns: ");
+    scanf("%d", &c);
+    
+    printf("Enter the matrix: \n");
+    for(int i=0; i<r; i++)
     {
-        for(int j=0; j<3; j++)
+        for (int j=0; j<c; j++)
         {
-            scanf("%d",&m[i][j]);
+            scanf("%d", &m[i][j]);
         }
     }
-    for(int i=0; i<3; i++)
+    
+    if(isSymmetric(m,r,c))
     {
-        for(int j=0; j<3; j++)
+        printf("The matrix is symmetric\n");
+        for(int i=0; i<r; i++)
         {
-            t[i][j] = m[j][i];
-        }
-    }
-    int S;
-    for(int i=0; i<3; i++)
-    {
-        for(int j=0; j<3; j++)
-        {
-            if(m[i][j]==t[i][j])
-            {
-                S = 1;
-            }
-            else
-            {
-                S = 0;
-            }
-        }
-    }
-    if(S==1)
-    {
-        printf("The given array is symmetric\n");
-        for(int i=0; i<3; i++)
-        {
-            for(int j=0; j<3; j++)
+            for (int j=0; j<c; j++)
             {
                 printf("%d ", m[i][j]);
             }
             printf("\n");
         }
     }
-    else
+    else 
     {
-        printf("The given array is symmetric\n");
-        for(int i=0; i<3; i++)
+        printf("The matrix is not symmetric\n");
+        for(int i=0; i<r; i++)
         {
-            for(int j=0; j<3; j++)
+            for (int j=0; j<c; j++)
             {
                 printf("%d ", m[i][j]);
             }
             printf("\n");
         }
     }
-}//end
+}
