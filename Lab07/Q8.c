a743ccc582f52ec0032b533c80485aebf79c9c89
iamunknowngamer <144406935+iamunknowngamer@users.noreply.github.com>
2023-10-27 06:24:36 -0700

    Update Q8.c

--- Lab07/Q8.c
+++ Lab07/Q8.c
@@ -1,29 +1,34 @@
 /* Programmer: Syed Saif ur Rehman Shah
 * Date: 10/10/2023
-* Description: This is the code for Question 8
+* Description: This is the code for Question 8(bubble sort method)
 * Related files: https://github.com/iamunknowngamer/Pf-Fall23/tree/main/Lab03
 */
 
 #include <stdio.h>
 
-int main()
-{
-    int N,temp,min;
+int main() {
+    int N;
     printf("Enter the size of array: ");
     scanf("%d", &N);
     int arr[N];
-    printf("Enter the elements of array: ");
-    
-    for(int i=0; i<N; i++) scanf("%d", arr[i]);
-    
-    for(int j=0; j<N; j++){
-        temp = arr[j];
-        for(int k=j+1; j<N; j++){
-            if(temp < arr[k]){
-                
+    printf("Enter the elements of the array:\n");
+    for (int i = 0; i < N; i++) scanf("%d", &arr[i]);
+    for (int i = 0; i < N - 1; i++) {
+        for (int j = 0; j < N - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
-    
-    
+    printf("Sorted array in ascending order:\n{");
+    for (int i = 0; i < N; i++) {
+        printf("%d", arr[i]);
+        if (i < N - 1) {
+            printf(", ");
+        }
+    }
+    printf("}\n");
 }
+
