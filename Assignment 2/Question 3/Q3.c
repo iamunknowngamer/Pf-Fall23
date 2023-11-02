246ee9829a33683b49dae74a3fae7ec2765fc669
iamunknowngamer <144406935+iamunknowngamer@users.noreply.github.com>
2023-11-02 06:43:23 -0700

    Update Q3.c

--- Assignment 2/Question 3/Q3.c
+++ Assignment 2/Question 3/Q3.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main() {
+    // Define the 2D array representing flight availability
     int availability[5][2] = {
         {1, 0},
         {1, 1},
@@ -8,8 +9,12 @@ int main() {
         {1, 0},
         {1, 1}
     };
+
+    // Define prices for morning and evening flights
     int morningPrice[5] = {300, 320, 280, 380, 375};
-    int eveningPrice[5] = {0, 310, 280, 0, 400};t.
+    int eveningPrice[5] = {0, 310, 280, 0, 400};
+
+    // 1. Analyze the flight availability for each day and time slot.
     int bestDay = -1;
     int bestTimeSlot = -1;
     int highestChance = 0;
@@ -34,6 +39,8 @@ int main() {
     printf("1. Best option: %s, %s time slot\n", 
            bestDay != -1 ? "Day " : "No available flights",
            bestTimeSlot == 0 ? "Morning" : "Evening");
+
+    // 2. The traveler prefers morning time slot.
     printf("2. Suggested day(s) for morning flight: ");
     for (int day = 0; day < 5; day++) {
         if (availability[day][0] == 1) {
@@ -41,14 +48,18 @@ int main() {
         }
     }
     printf("\n");
+
+    // 3. The traveler prefers evening time slot.
     printf("3. Suggested day(s) for evening flight: ");
     for (int day = 0; day < 5; day++) {
         if (availability[day][1] == 1) {
             printf("Day %d, ", day + 1);
         }
     }
-    printf("\n");y.
-    int specificDay = 3;
+    printf("\n");
+
+    // 4. Inquiring about flights on a specific day.
+    int specificDay = 3; // Change this to the desired day (0-4)
     if (availability[specificDay][0] == 1) {
         printf("4. Flights available on Day %d (Morning) for $%d\n", specificDay + 1, morningPrice[specificDay]);
     } else if (availability[specificDay][1] == 1) {
