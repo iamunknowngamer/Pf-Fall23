af03f3768baf1d8704fd1b8d292cad64b3ff2526
iamunknowngamer <144406935+iamunknowngamer@users.noreply.github.com>
2023-11-04 23:03:10 -0700

    Update Q9.c

--- Lab07/Q9.c
+++ Lab07/Q9.c
@@ -5,40 +5,40 @@
 
 #include <stdio.h>
 
-int main() {
-    int X, Y;
-    printf("Enter the number of company branches: ");
-    scanf("%d", &X);
-    printf("Enter the number of mobile phones per branch: ");
-    scanf("%d", &Y);
-    double bills[X][Y];
-    double totalBill = 0.0;
-    double maxBill = 0.0;
-    int branchWithMaxBill = -1;
-    int maxBillBranchID = -1;
-    int maxBillMobilePhoneID = -1;
-    for (int i = 0; i < X; i++) {
-        printf("Enter bill amounts for branch %d:\n", i+1);
-        for (int j = 0; j < Y; j++) {
-            printf("Enter bill amount for mobile phone %d: ", j+1);
-            scanf("%lf", &bills[i][j]);
-            totalBill += bills[i][j];
-            if (bills[i][j] > maxBill) {
-                maxBill = bills[i][j];
-                branchWithMaxBill = i;
-                maxBillBranchID = i;
-                maxBillMobilePhoneID = j;
+int main()
+{
+    int x,y,bt,total = 0,max = 0,branch_id,cell_id;
+    printf("Enter the number of branches: ");
+    scanf("%d", &x);
+    printf("Enter the number of cell phones per branch: ");
+    scanf("%d", &y);
+    int data[x][y];
+    for(int i=0; i<x; i++)
+    {
+        printf("\nBills of branch %d: ", i+1);
+        for(int j=0; j<y; j++)
+        {
+            printf("\nEnter the bill for cell phone %d: ", j+1);
+            scanf("%d", &data[i][j]);
+            total += data[i][j];
+            if(max < data[i][j])
+            {
+            max = data[i][j];
+            branch_id = i+1;
+            cell_id = j+1;
             }
         }
-    }
-    printf("Total bill for all branches: %.2lf\n", totalBill);
-    for (int i = 0; i < X; i++) {
-        double branchTotal = 0.0;
-        for (int j = 0; j < Y; j++) {
-            branchTotal += bills[i][j];
+    } 
+    printf("Total bill of all branches: %d\n", total);
+    for(int i=0; i<x; i++)
+    {
+        bt = 0;
+        for(int j=0; j<y; j++)
+        {
+            bt += data[i][j];
         }
-        printf("Total bill for branch %d: %.2lf\n", i+1, branchTotal);
+        printf("Branch %d total: %d\n",i+1,bt);
     }
-    printf("Branch ID where maximum bill arrived: %d\n", branchWithMaxBill+1);
-    printf("Branch and Mobile Phone IDs where bill is highest: Branch %d, Mobile Phone %d\n", maxBillBranchID+1, maxBillMobilePhoneID+1);
+    printf("Branch where max bill arrived: %d\n", branch_id);
+    printf("Branch and Mobile Phone IDs where bill is highest: Branch %d, Cell phone %d.\n",branch_id,cell_id);
 }
