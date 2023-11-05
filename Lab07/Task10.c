72c485b46c3358d41365a47e9042f405d66b939b
iamunknowngamer <144406935+iamunknowngamer@users.noreply.github.com>
2023-11-04 23:29:34 -0700

    Update Task10.c

--- Lab07/Task10.c
+++ Lab07/Task10.c
@@ -5,35 +5,40 @@
 
 #include <stdio.h>
 
-int main() {
-    int N, S;
-    printf("Enter Number S: ");
-    scanf("%d", &S);
-    printf("Enter the size of the array: ");
-    scanf("%d", &N);
-    int A[N];
-    printf("Enter the elements of the array: ");
-    for (int i = 0; i < N; i++) {
-        scanf("%d", &A[i]);
-    }
-    int start = 0, end = 0;
-    int sum = 0;
-    int found = 0;
-    for (int i = 0; i < N; i++) {
+void check(int N, int A[], int S)
+{
+    int start = 0, end = 0, sum = 0, found = 0;
+    for(int i=0; i<N; i++)
+    {
         sum += A[i];
-        while (sum > S) {
+        while (sum > S) 
+        {
             sum -= A[start];
             start++;
         }
-        if (sum == S) {
+        if(sum == S)
+        {
             end = i;
             found = 1;
             break;
         }
     }
-    if (found) {
-        printf("The Elements from Index %d to %d when summed results in the output of %d.\n", start, end, S);
-    } else {
-        printf("No subarray found with the given sum %d.\n", S);
-    }
+    if(found) printf("The Elements from Index %d to %d when summed results in the output of %d.\n", start, end, S);
+    else printf("No subarray found!");
+}
+int main()
+{
+    int N,sum=0;
+    printf("Enter the size of array: ");
+    scanf("%d", &N);
+    
+    int A[N];
+    printf("Enter the elements of the array: ");
+    for(int i=0; i<N; i++) scanf("%d", &A[i]);
+    
+    int S;
+    printf("Specify a value for S: ");
+    scanf("%d", &S);
+    
+    check(N,A,S);
 }
