122b873474b3e7592508c36d4800b2f0d6d39a22
iamunknowngamer <144406935+iamunknowngamer@users.noreply.github.com>
2023-11-19 20:35:13 +0500

    Update Q4.c

--- Lab09/Recursions/Q4.c
+++ Lab09/Recursions/Q4.c
@@ -1,5 +1,5 @@
 /* Programmer: Syed Saif ur Rehman Shah
-* Description: This is the code for Question 1
+* Description: This is the code for Question 4
 * Related files: https://github.com/iamunknowngamer/Pf-Fall23/blob/main/Lab06/Q6.c
 */
 
