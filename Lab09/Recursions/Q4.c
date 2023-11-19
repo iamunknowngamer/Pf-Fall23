f8ae40d7e136462edf3540f835a32644a24ac679
iamunknowngamer <144406935+iamunknowngamer@users.noreply.github.com>
2023-11-19 20:32:50 +0500

    Update Q4.c

--- Lab09/Recursions/Q4.c
+++ Lab09/Recursions/Q4.c
@@ -1,13 +1,15 @@
+/* Programmer: Syed Saif ur Rehman Shah
+* Description: This is the code for Question 1
+* Related files: https://github.com/iamunknowngamer/Pf-Fall23/blob/main/Lab06/Q6.c
+*/
+
 #include <stdio.h>
 
-int find(int N)
-{
+int find(int N){
     if(N<=1) return N;
     else return find(N-1) + find(N-2);
-    
 }
-int main()
-{
+int main(){
     int N;
     printf("Enter a number : ");
     scanf("%d", &N);
