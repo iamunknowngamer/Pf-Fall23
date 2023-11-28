6eae0f7817432be05a2103633fa08186fc6612c7
iamunknowngamer <144406935+iamunknowngamer@users.noreply.github.com>
2023-11-28 10:09:31 +0500

    Update Q1.c

--- Lab12/Q1.c
+++ Lab12/Q1.c
@@ -1,1 +1,5 @@
+/* Programmer: Syed Saif ur Rehman Shah
+* Description: You can customzie the file as you want and it will still work but you need to place commas, this program will display an excel format sheet (table or data).
+* Related files: https://github.com/iamunknowngamer/Pf-Fall23/blob/main/Lab06/Q6.c
+*/
 
