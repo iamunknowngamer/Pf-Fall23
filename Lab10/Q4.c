78ed287c6db06586a6d28dcec3fbdf846e44e0e1
Syed Saif ur Rehman Shah <144406935+iamunknowngamer@users.noreply.github.com>
2023-12-07 14:31:32 +0500

    Update Q4.c

--- Lab10/Q4.c
+++ Lab10/Q4.c
@@ -1,1 +1,43 @@
+/* Programmer: Syed Saif ur Rehman Shah
+* Description: Printing the
+* Related files: https://github.com/iamunknowngamer/Pf-Fall23/blob/main/Lab06/Q6.c
+*/
 
+#include <stdio.h>
+#include <string.h>
+
+void reverseWord(char* start, char* end){
+    while (start < end){
+        char temp = *start;
+        *start = *end;
+        *end = temp;
+        start++;
+        end--;
+    }
+}
+
+void reverseWords(char* sentence){
+    char* start = sentence;
+    char* end = sentence;
+    
+    while (*end != '\0'){
+        if (*end == ' '){
+            reverseWord(start, end - 1);
+            start = end + 1;
+        }
+        end++;
+    }
+    reverseWord(start, end - 1);
+}
+
+int main(){
+    char sentence[1000];
+    
+    printf("Enter a sentence: ");
+    fgets(sentence, sizeof(sentence), stdin);
+
+    sentence[strcspn(sentence, "\n")] = '\0';
+    reverseWords(sentence);
+
+    printf("Modified Sentence: %s\n", sentence);
+}
