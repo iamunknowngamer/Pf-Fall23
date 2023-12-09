3800fcf64323b5de3dbc3748219aa9f250822260
Syed Saif ur Rehman Shah <144406935+iamunknowngamer@users.noreply.github.com>
2023-12-09 11:20:54 +0500

    Update Q8.c

--- Lab11/Q8.c
+++ Lab11/Q8.c
@@ -12,16 +12,16 @@ typedef struct Box {
 } Box;
 
 int main() {
-  int num_boxes;
+  int n;
   printf("Enter the number of boxes: ");
-  scanf("%d", &num_boxes);
-  Box boxes[num_boxes];
-  for (int i = 0; i < num_boxes; i++) {
+  scanf("%d", &n);
+  Box boxes[n];
+  for (int i = 0; i < n; i++) {
     printf("Enter the length, width and height of box %d: ", i + 1);
     scanf("%d %d %d", &boxes[i].length, &boxes[i].width, &boxes[i].height);
   }
   printf("Boxes that can pass through the tunnel:\n");
-  for (int i = 0; i < num_boxes; i++) {
+  for (int i = 0; i < n; i++) {
     if (boxes[i].height < 41) {
       printf("Box %d: volume = %d\n", i + 1, boxes[i].length * boxes[i].width * boxes[i].height);
     }
